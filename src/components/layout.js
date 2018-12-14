72cca7a50464810569df74c258f8b0fbe8436545
Ryan Kubik <rmkubik@me.com>
2018-12-14 11:34:34 -0800

    Move layout.css -> theme.css and create layout css module for layout specific css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Header from './header'
-import './layout.css'
+import Header from './header';
+import styles from './layout.module.css';
+import './theme.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,23 +20,14 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <div className={styles.body}>{children}</div>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
