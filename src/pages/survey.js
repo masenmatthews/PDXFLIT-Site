ab6877e8440c6d2a090ed82a0dcea234eddc1dfa
Ryan Kubik <rmkubik@me.com>
2018-12-14 12:07:07 -0800

    Create empty shell pages for about, contact, survey

---
+++ src/pages/survey.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Survey = () => (
+  <Layout>
+    <SEO title="Survey" />
+  </Layout>
+);
+
+export default Survey;
