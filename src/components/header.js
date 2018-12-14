b4d61e042649885df6c64b5f9cd62e49092dacd4
Ryan Kubik <rmkubik@me.com>
2018-12-14 12:28:28 -0800

    Convert header styles to sass and apply them to new links

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,19 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import styles from './header.module.css';
+import styles from './header.module.scss';
 
 const Header = ({ siteTitle }) => (
   <div className={styles.background}>
     <div className={styles.container}>
       <h1 className={styles.header}>
-        <Link className={styles.link} to="/">
-          {siteTitle}
-        </Link>
+        <Link to="/">{siteTitle}</Link>
       </h1>
+      <div className={styles.links}>
+        <Link to="/about/">About</Link>
+        <Link to="/contact/">Contact</Link>
+        <Link to="/survey/">New Member Survey</Link>
+      </div>
     </div>
   </div>
 );
