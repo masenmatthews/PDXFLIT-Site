56ad44a583aec5b733eeeae615d332bf649e6abd
Ryan Kubik <rmkubik@me.com>
2018-12-14 11:56:49 -0800

    Use new pdxFlitLogo image and move styles to css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import Image from '../components/images/pdxFlitLogo';
+import SEO from '../components/seo';
+import styles from './index.module.css';
 
 const IndexPage = () => (
   <Layout>
@@ -11,11 +12,11 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    <div className={styles.mainImage}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
