5eb44a708d5f8cff3178f5a6a70251f57039cd07
Ryan Kubik <rmkubik@me.com>
2018-12-14 12:04:34 -0800

    Update index copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This is a place for people who aspire to work in PDXtech!</p>
+    <p>
+      We're creating an online community that can meet offline and work on
+      developing their skills!
+    </p>
     <div className={styles.mainImage}>
       <Image />
     </div>
